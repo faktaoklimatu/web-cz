3292b30c5ee143bcc8f3ac55f6ed5913ac58e843
Martin Ukrop <mukrop@mail.muni.cz>
2019-07-23 17:42:51 +0200

    Add automatic non-breaking space after prepositions
    * Resolves #48.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,9 @@ $(document).ready(function(){
             $(".navbar").removeClass('navbar-scrolled');
         }
     });
-
+});
+// Automatically replace spaces after prepositions with non-breakable spaces
+$(document).ready(function(){
+    var x = document.querySelector("body");
+    x.innerHTML = x.innerHTML.replace(/ ([ai]|[kosuvz]|do|ke|na|od|po|se|ve|za|ze) /ig, " $1&nbsp;");
 });
