812c290bdc0f2c96c3f54a735adca0c4c7647d6a
Martin Ukrop <mukrop@mail.muni.cz>
2019-08-07 10:46:48 +0200

    Separate web version as separate script
    * Resolves #74.

---
+++ utils/web-version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "usage: "$0" <repo URL>"
+    exit -1
+fi
+
+if [ ! $# -eq 1 ]
+then
+    echo "Error: Incorrect number of parameters (exactly 1 parameter required)."
+    exit -1
+fi
+
+REPO_URL=$1
+
+echo -n 'Last change: <a target="_blank" '
+echo -n 'href="'$REPO_URL'/commit/'`git rev-parse HEAD`'" '
+echo -n 'title="commit '`git rev-parse --short HEAD`'">'
+echo -n `git log -1 --date=short --format=%cd`
+echo '</a>'
