e8e30f83dd2f5262067a7412f6f8e8efb7a79fb0
Jan Drábek <jan@drabek.cz>
2019-08-20 20:55:21 +0200

    Skryti menu pri skrolovani na zarizenich s mensi vyskou plochy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // change styling of navbar if the page is scrolled
     $(document).scroll(function() {
         var scrolled = $(this).scrollTop();
         if(scrolled > 50) {
@@ -6,6 +7,23 @@ $(document).ready(function(){
         } else {
             $(".navbar").removeClass('navbar-scrolled');
         }
+    });
+
+    // hide navbar if the screen is small and the page is scrolled
+    var prevScrollpos = window.pageYOffset;
+    $(document).scroll(function () {
+        console.log($(window).height());
+        if ($(window).height() > 741) { // iPhone 6 plus / Galaxy S9 screen viewport height
+            $(".navbar").css('top', 0);
+            return;
+        }
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            $(".navbar").css('top', 0);
+        } else {
+            $(".navbar").css('top', '-56px');
+        }
+        prevScrollpos = currentScrollPos;
     });
 });
 // Automatically replace spaces after prepositions with non-breakable spaces
