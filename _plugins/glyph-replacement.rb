5eb9b5a9ae822eb6ee2d72444e991ee986a6455d
Martin Ukrop <mukrop@mail.muni.cz>
2019-11-13 18:23:26 +0100

    Pridane uzke medzery ako oddelovac tisicov
    Resolves #215.

--- _plugins/glyph-replacement.rb
+++ _plugins/glyph-replacement.rb
@@ -10,7 +10,7 @@ end
 
 def replace(content)
   content.gsub!(/ ([ai]|[kosuvz]) /i, ' \1&nbsp;')
-  content.gsub!(/([0-9]) (000)/, '\1&thinsp;\2')
+  content.gsub!(/([0-9]) ([0-9]{3})/, '\1&thinsp;\2')
   content.gsub!(/([0-9]) (%)/, '\1&thinsp;\2')
   content.gsub!(/([0-9]) (°C|ppm|kg|mil\.|tCO)/, '\1&nbsp;\2')
   content.gsub!(/([0-9]) (W|Wh|kW|MW|GW|kWh|MWh|GWh)/, '\1&nbsp;\2')
