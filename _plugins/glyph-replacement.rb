ddd14508331c0c9ffcac8d6bb17ac9fea61992c0
Martin Ukrop <mukrop@mail.muni.cz>
2019-11-29 22:17:29 +0100

    Drobné opravy
    * Přidána mezera před procenta.
    * Dodrženy konvenve MD souborů (volné řádky a pod.).
    * Před TWh se nyní doplňuje nezlomitelná mezera.

--- _plugins/glyph-replacement.rb
+++ _plugins/glyph-replacement.rb
@@ -13,7 +13,7 @@ def replace(content)
   content.gsub!(/([0-9]) ([0-9]{3})/, '\1&thinsp;\2')
   content.gsub!(/([0-9]) (%)/, '\1&thinsp;\2')
   content.gsub!(/([0-9]) (°C|ppm|kg|mil\.|tCO)/, '\1&nbsp;\2')
-  content.gsub!(/([0-9]) (W|Wh|kW|MW|GW|kWh|MWh|GWh)/, '\1&nbsp;\2')
+  content.gsub!(/([0-9]) (W|Wh|kW|MW|GW|kWh|MWh|GWh|TWh)/, '\1&nbsp;\2')
 
   content
 end
