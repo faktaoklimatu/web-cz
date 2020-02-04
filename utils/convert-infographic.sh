3a6eb7840c1605ac4651877fa58ca8c669fd5dae
Martin Ukrop <mukrop@mail.muni.cz>
2020-02-04 22:31:51 +0100

    Oprava v konverzi nového Inkscape
    Je potřeba explicitně nastavit průhlednost pozadí.

--- utils/convert-infographic.sh
+++ utils/convert-infographic.sh
@@ -41,19 +41,19 @@ INKSCAPE_v1_0=`inkscape --version | grep -F 1. | wc -l`
 if [ $INKSCAPE_v1_0 -eq 1 ]
 then
     echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_600 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=600 \
+    inkscape --without-gui --export-area-page --export-background=white --export-background-opacity=255 --export-width=600 \
             --export-type=png --export-file=$DST_FILE_PNG_600 $DST_FILE_SVG 1>/dev/null 2>&1
     echo -e "\t\t [ OK ]"
     echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_1200 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=1200 \
+    inkscape --without-gui --export-area-page --export-background=white --export-background-opacity=255 --export-width=1200 \
             --export-type=png --export-file=$DST_FILE_PNG_1200 $DST_FILE_SVG 1>/dev/null 2>&1
     echo -e "\t\t [ OK ]"
     echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_1920 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=1920 \
+    inkscape --without-gui --export-area-page --export-background=white --export-background-opacity=255 --export-width=1920 \
             --export-type=png --export-file=$DST_FILE_PNG_1920 $DST_FILE_SVG 1>/dev/null 2>&1
     echo -e "\t\t [ OK ]"
     echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_6000 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=6000 \
+    inkscape --without-gui --export-area-page --export-background=white --export-background-opacity=255 --export-width=6000 \
             --export-type=png --export-file=$DST_FILE_PNG_6000 $DST_FILE_SVG 1>/dev/null 2>&1
     echo -e "\t\t [ OK ]"
 else
