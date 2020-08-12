81a53d2f9e9e508c04d84ffbb5e4545421254fa1
Matěj Grabovský <matej@mgrabovsky.net>
2020-08-12 15:13:28 +0200

    Vylepšení skriptu pro konverzi infografik
    * Redukce opakování kódu (DRY)
    * Různé drobné Bashové triky

--- utils/convert-infographic.sh
+++ utils/convert-infographic.sh
@@ -1,80 +1,74 @@
 #!/bin/bash
 
-# Check CLI parameters
-if [ $# -eq 0 ]
-then
-    echo "usage: "$0" <source/infographic.pdf> <destination/infographic.pdf>"
-    exit -1
-fi
+# Validate command line arguments
 if [ ! $# -eq 2 ]
 then
     echo "Error: Incorrect number of parameters (exactly 2 parameters required)."
-    exit -1
+    echo "Usage: "$0" <source/infographic.pdf> <destination/infographic.pdf>"
+    exit 1
 fi
 
-# exit when any command fails
+# Exit when any command fails
 set -e
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command exited with code $?."' EXIT
 
-# Set names
+# Widths (in px) of infographics we want to generate in PNG
+WIDTHS=(600 1200 1920 6000)
+
+# Set up file names
 SRC_FILE_PDF=$1
 DST_FILE_PDF=$2
-DST_FILE_SVG=`echo $DST_FILE_PDF | sed 's/\.pdf$/\.svg/'`
-DST_FILE_PNG_600=`echo $DST_FILE_PDF | sed 's/\.pdf$/\_600.png/'`
-DST_FILE_PNG_1200=`echo $DST_FILE_PDF | sed 's/\.pdf$/\_1200.png/'`
-DST_FILE_PNG_1920=`echo $DST_FILE_PDF | sed 's/\.pdf$/\_1920.png/'`
-DST_FILE_PNG_6000=`echo $DST_FILE_PDF | sed 's/\.pdf$/\_6000.png/'`
+DST_FILE_SVG=${DST_FILE_PDF%.pdf}.svg
 
-# Creat destination folder if necessary
-mkdir -p `dirname $DST_FILE_PDF`
+# Create destination folder if necessary
+mkdir -p $(dirname $DST_FILE_PDF)
 
-# Do SVG and PNG conversion
-echo -n "Converting $SRC_FILE_PDF to $DST_FILE_SVG ..."
-pdf2svg $SRC_FILE_PDF $DST_FILE_SVG 1>/dev/null 2>&1
-echo -e "\t\t [ OK ]"
+# Convert original PDF to SVG
+echo -n "Converting $SRC_FILE_PDF to SVG..."
+pdf2svg "$SRC_FILE_PDF" "$DST_FILE_SVG" >/dev/null 2>&1
+echo " OK"
 
-INKSCAPE_v1_0=`inkscape --version | grep -F 1. | wc -l`
-
-if [ $INKSCAPE_v1_0 -eq 1 ]
-then
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_600 ..."
-    inkscape --export-area-page --export-background=white --export-background-opacity=255 --export-width=600 \
-            --export-type=png --export-filename=$DST_FILE_PNG_600 $DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_1200 ..."
-    inkscape --export-area-page --export-background=white --export-background-opacity=255 --export-width=1200 \
-            --export-type=png --export-filename=$DST_FILE_PNG_1200 $DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_1920 ..."
-    inkscape --export-area-page --export-background=white --export-background-opacity=255 --export-width=1920 \
-            --export-type=png --export-filename=$DST_FILE_PNG_1920 $DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_6000 ..."
-    inkscape --export-area-page --export-background=white --export-background-opacity=255 --export-width=6000 \
-            --export-type=png --export-filename=$DST_FILE_PNG_6000 $DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-else
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_600 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=600 \
-            --export-png=$DST_FILE_PNG_600 --file=$DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_1200 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=1200 \
-            --export-png=$DST_FILE_PNG_1200 --file=$DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_1920 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=1920 \
-            --export-png=$DST_FILE_PNG_1920 --file=$DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
-    echo -n "Converting $DST_FILE_SVG to $DST_FILE_PNG_6000 ..."
-    inkscape --without-gui --export-area-page --export-background=white --export-width=6000 \
-            --export-png=$DST_FILE_PNG_6000 --file=$DST_FILE_SVG 1>/dev/null 2>&1
-    echo -e "\t\t [ OK ]"
+INKSCAPE_VERSION_1=
+if inkscape --version 2>/dev/null | grep '^Inkscape 1\.' >/dev/null; then
+    INKSCAPE_VERSION_1=1
 fi
 
+convert_svg_to_png() {
+    input_svg=$1
+    width=$2
+
+    # Different options for Inkscape 1.x
+    if [ $INKSCAPE_VERSION_1 ]; then
+        inkscape \
+            --export-area-page \
+            --export-background=white \
+            --export-width=$width \
+            --export-background-opacity=255 \
+            --export-type=png \
+            --export-filename="${input_svg%.svg}_$width.png" \
+            "$input_svg" \
+            >/dev/null 2>&1
+    else
+        inkscape \
+            --export-area-page \
+            --export-background=white \
+            --export-width=$width \
+            --without-gui \
+            --export-png="${input_svg%.svg}_$width.png" \
+            --file="$input_svg" \
+            >/dev/null 2>&1
+    fi
+}
+
+# Convert SVG into PNGs of various sizes
+echo "Converting $DST_FILE_SVG to PNG:"
+for width in ${WIDTHS[@]}; do
+    echo -en "    Width ${width}px..."
+    convert_svg_to_png "$DST_FILE_SVG" $width
+    echo " OK"
+done
+
 # If all previous conversions pass, copy the file checked by Make
-echo "Copying $SRC_FILE_PDF as $DST_FILE_PDF"
-cp $SRC_FILE_PDF $DST_FILE_PDF
+echo "Copying $SRC_FILE_PDF to $DST_FILE_PDF"
+cp "$SRC_FILE_PDF" "$DST_FILE_PDF"
+
+echo "All done"
