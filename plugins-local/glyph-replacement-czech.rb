fe7ff5595ec373603ee63c42266d4e12fef3ab0d
Martin Ukrop <mukrop@mail.muni.cz>
2021-04-23 10:46:20 +0200

    Vylepšeno nahrazování nezlomitelných mezer
    Za jednopísmenovou spojkou/předložkou se vkládá jednopísmenová mezera i když je to první věc v závorce.
    Aktualizován web-core (oprava nahrazování mezer tam).

--- plugins-local/glyph-replacement-czech.rb
+++ plugins-local/glyph-replacement-czech.rb
@@ -3,17 +3,19 @@
 Jekyll::Hooks.register :site, :post_render do |site|
   Jekyll.logger.info  "                  * Replacing non-breaking spaces (Czech) ..."
   
+  def replace!(content)
+    # One-letter conjunctions and prepositions should not be left hanging.
+    content.gsub!(/([ \()])([aikosuvz]) /i, '\1\2&nbsp;')
+    # Nonbreaking space after common abbreviations with and without period after space or opening parenthesis.
+    content.gsub!(/([ \()])(srov|tzn|tzv|tj|vs|např|popř|č|mj|zvl|r|str|angl)\. /i, '\1\2.&nbsp;')
+    content.gsub!(/([ \()])(viz|cca) /i, '\1\2&nbsp;')
+    # Nonbreaking space before common ending abbreviations before space or end parenthesis.
+    content.gsub!(/ (aj|atd|apod)\.([ \)])/i, '&nbsp;\1.\2')
+  end
+
   site.documents.each do |page|
+    Jekyll.logger.error "Undefined page.output for '#{page.path}'. Consider excluding this file" if not page.respond_to? :output
     replace!(page.output)
   end
-end
 
-def replace!(content)
-  # One-letter conjunctions and prepositions should not be left hanging.
-  content.gsub!(/ ([aikosuvz]) /i, ' \1&nbsp;')
-  # Nonbreaking space after common abbreviations with and without period after space or opening parenthesis.
-  content.gsub!(/([ \()])(srov|tzn|tzv|tj|vs|např|popř|č|mj|zvl|r|str|angl)\. /i, '\1\2.&nbsp;')
-  content.gsub!(/([ \()])(viz|cca) /i, '\1\2&nbsp;')
-  # Nonbreaking space before common ending abbreviations before space or end parenthesis.
-  content.gsub!(/ (aj|atd|apod)\.([ \)])/i, '&nbsp;\1.\2')
 end
