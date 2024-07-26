02079a0b48dccb7d08f071d58e29071e7cb670b2
Matěj Grabovský <matej@mgrabovsky.net>
2024-07-26 21:33:50 +0200

    Technické review kalkulačky ETS 2

---
+++ assets-local/js/ets2-kalkulacka.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const formatNumber = Intl.NumberFormat('cs-CZ', { maximumFractionDigits: 1 }).format;
+
+  const $allowancePrice = $('#allowance-price-number');
+  const $allowancePriceInput = $('#allowance-input-range');
+  const $exchangeRate = $('#exchange-rate');
+  const exchangeRate = Number($exchangeRate.text());
+
+  // List of fuels displayed in calculator.
+  const $fuels = $('.fuel');
+
+  const updatePrices = () => {
+    const allowancePriceEur = Number($allowancePriceInput.val());
+
+    // Update allowance price in local currency.
+    const allowancePriceLocal = allowancePriceEur * exchangeRate;
+    $allowancePrice.text(allowancePriceEur);
+    $('#allowance-price-czk').text(formatNumber(allowancePriceLocal));
+
+    $fuels.each((_i, fuelRow) => {
+      const $priceIncrease = $('.price-increase', fuelRow);
+      const $percentage = $('.price-increase-percent', fuelRow);
+      const fuelPrice = Number($('.fuel-price', fuelRow).text());
+      const carbonContent = fuelRow.dataset.carbonContent;
+
+      // Update price increase and price increase percentage components.
+      const priceIncrease = allowancePriceLocal * carbonContent / 1000;
+      const priceIncreasePercent = 100 * priceIncrease / fuelPrice;
+
+      $priceIncrease.text(`+${formatNumber(priceIncrease)}`);
+      $percentage.text(formatNumber(priceIncreasePercent));
+    });
+  };
+
+  $allowancePriceInput.on('input', () => {
+    updatePrices();
+  });
+
+  updatePrices();
+});
